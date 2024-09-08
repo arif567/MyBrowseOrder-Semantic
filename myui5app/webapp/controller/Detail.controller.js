dbd348694bedb8bc6b874c3791b1c9b28c7897e5
Arif <fmarif2009@gmail.com>
2024-09-08 09:50:07 +0700

    end

--- myui5app/webapp/controller/Detail.controller.js
+++ myui5app/webapp/controller/Detail.controller.js
@@ -1,15 +1,98 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "./BaseController",
+    "../model/formatter",
+    "sap/m/library",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (BaseController, formatter,mobileLibrary,JSONModel) {
         "use strict";
 
-        return Controller.extend("myui5app.controller.Detail", {
+        var URLHelper = mobileLibrary.URLHelper;
+
+        return BaseController.extend("myui5app.controller.Detail", {
+            formatter: formatter,
             onInit: function () {
 
+                this._aValidKeys =["shipping","processor"];
+                var oViewModel = new JSONModel(
+                    {
+                        busy: false,
+                        delay: 0,
+                        currency: "EUR",
+                        selectedTab: ""
+                    }
+
+
+                );
+                this.setModel(oViewModel,"detailView");
+
+
+                this.getRouter().getRoute("RouteDetail").attachPatternMatched(this._onObjectMatched, this);
+            },
+
+            onSendEmailPress :function(){
+                URLHelper.triggerEmail(
+
+                );
+
+            },
+
+
+            _onObjectMatched: function (oEvent) {
+                this.getModel("appView").setProperty("/layout", "TwoColumnsMidExpanded");
+
+                var oArguments = oEvent.getParameter("arguments");
+                 this._OrderNo = oArguments.OrderNo;
+                
+                this.getModel().metadataLoaded().then(function () {
+                   
+                    var OrderPath = this.getModel().createKey("ZA_BO_SalesOrder_Header", {
+                        OrderNo: this._OrderNo
+                    });
+                    
+                    console.log("OrderPath:", OrderPath);
+                    
+                    this.getView().bindElement({
+                        path: "/" + OrderPath,
+                        parameters: {
+                            expand: "to_LineItems,to_ShippingAddress"
+                        },
+                        events: {
+                            dataReceived: function (oData) {
+                                var data = oData.getParameter("data");
+                                console.log("Full data received: ", data);
+                               
+                            }
+                        }
+                    });
+                }.bind(this));
+
+                var oQuery = oArguments["?query"];
+                if (oQuery && this._aValidKeys.indexOf(oQuery.tab) >= 0){
+                    this.getView().getModel("detailView").setProperty("/selectedTab", oQuery.tab);
+                    this.getRouter().getTargets().display(oQuery.tab);
+                } else {
+                    this.getRouter().navTo("RouteDetail", {
+                        OrderNo : this._OrderNo,
+                        query: {
+                            tab: "shipping"
+                        }
+                    }, true);
+                }
+            },
+
+            onTabSelect: function(oEvent){
+                var sSelectedTab = oEvent.getParameter("selectedKey");
+                
+                this.getRouter().navTo("RouteDetail",{
+                    OrderNo : this._OrderNo,
+                    query: {
+                        tab : sSelectedTab
+                    }
+                },true);
             }
         });
     });
--- myui5app/webapp/controller/Master.controller.js
+++ myui5app/webapp/controller/Master.controller.js
@@ -1,19 +1,57 @@
 sap.ui.define([
     "./BaseController",
-    "../model/formatter"
+    "../model/formatter",
+    "sap/ui/Device",
 
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (BaseController,formatter) {
+    function (BaseController,formatter,Device) {
         "use strict";
 
         return BaseController.extend("myui5app.controller.Master", {
             formatter: formatter,
             onInit: function () {
                 console.log("allah");
+                this.getRouter().getRoute("RouteMaster").attachPatternMatched(this._onMasterMatched, this);
 
-            }
+            },
+            createGroupHeader : function (oGroup) {
+                return new GroupHeaderListItem({
+                    title : oGroup.text
+                });
+            },
+            onSelectionChange : function(oEvent)
+            {
+                var oList = oEvent.getSource(),
+                    bSelected = oEvent.getParameter("selected"),
+                    listItem = oEvent.getParameter("listitem");
+                    this._showDetail(listItem || oEvent.getSource());
+                    console.log(listItem);
+
+                
+            },
+
+            _showDetail : function (oItem){
+                var bReplace = !Device.system.phone;
+              var  OrderNo = oItem.getBindingContext().getProperty("OrderNo");
+
+              console.log("OrderNo: ", OrderNo);
+             // OrderNo ='0000005579'
+
+              this.getRouter().navTo("RouteDetail",{
+                OrderNo: OrderNo
+              },bReplace);
+
+              
+
+
+              
+            },
+            _onMasterMatched :  function() {
+                //Set the layout property of the FCL control to 'OneColumn'
+                this.getModel("appView").setProperty("/layout", "OneColumn");
+            },
         });
     });
